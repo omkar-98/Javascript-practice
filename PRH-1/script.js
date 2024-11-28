4721e2a6a51a7e21deaac1f1c4da26f5cc7b79e4
omkar <omkardaptare936@gmail.com>
2024-11-28 22:41:06 +0530

    Problem-1

---
+++ PRH-1/script.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector("button");
+var para = document.querySelector("p");
+
+document.addEventListener("click",function(){
+    para.innerText = "The text has been changed"
+});
